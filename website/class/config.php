dba18ec0c0fdfb7c34453ea6c67d89ad63943770
svetersen89 <93587989+svetersen89@users.noreply.github.com>
2024-07-12 10:48:34 +0200

    Update config.php

--- website/class/config.php
+++ website/class/config.php
@@ -1,14 +1,13 @@
 <?php
-define('DB_HOST', 'charred.dathost.net');
+define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');
-define('DB_NAME', '647896ed0407c5f75bcc0cd9');
-define('DB_USER', 'ZHiW7QQHxfdbzAzZ');
-define('DB_PASS', 'KnXYeLilNP');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASS', '');
 
 define('WEB_STYLE_DARK', true);
 
-define('STEAM_API_KEY', 'test');
+define('STEAM_API_KEY', '');
 define('STEAM_DOMAIN_NAME', '');
 define('STEAM_LOGOUT_PAGE', '');
 define('STEAM_LOGIN_PAGE', '');
-
